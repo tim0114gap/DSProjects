8df4b8f604ff6b037377776dcad7ce2f0dab78ee
tim0114gap <tim.0114.gap@gmail.com>
2019-11-19 01:24:01 +0900

    changed

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,4 +132,4 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
