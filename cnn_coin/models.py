8db5dc9f9457023b6f349d4aefe9d5eca2e99037
tim0114gap <tim.0114.gap@gmail.com>
2019-11-20 00:24:52 +0900

    changed

--- cnn_coin/models.py
+++ cnn_coin/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ImageModel(models.Model):
-    image = models.ImageField(null = True)
+    image = models.ImageField(null = True, upload_to='cnn_coin')
 
     def __str__(self):
         return self.image.url
